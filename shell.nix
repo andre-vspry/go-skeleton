12efd7fc44f1cd5a7fbbc53b34c957202ceb7a3d
Andre Perry <andre@vspry.com>
2022-06-22 23:57:28 +0000

    feat: use home-manager go tools

--- shell.nix
+++ shell.nix
@@ -2,17 +2,12 @@
 , unstable ? import <unstable> { }
 }:
 let
-  go-jwt = unstable.callPackage ./nix/go-jwt {};
-  go-enum = unstable.callPackage ./nix/go-enum {};
-  go-tools = unstable.callPackage ./nix/go-tools {};
-  gojg = unstable.callPackage ./nix/gojq {};
   # gqlgen = unstable.callPackage ./nix/gqlgen {}; # Uses plugins which are not working
   # just run `go run github.com/99designs/gqlgen@latest`
 in
 pkgs.mkShell {
   packages = with pkgs; [
-  ] ++ (with unstable; [
-    go gopls gopkgs gotests gomodifytags impl go-tools delve goplay goreleaser go-mockery go-outline go-bindata golangci-lint gofumpt
+    go_1_18 gopls gopkgs gotests gomodifytags impl go-tools delve goplay goreleaser go-mockery go-outline go-bindata golangci-lint gofumpt
     go-jwt
     go-enum
     openssl
@@ -22,5 +17,6 @@ pkgs.mkShell {
     google-cloud-sdk
     # skaffold minikube kubectl
     sonar-scanner-cli
+  ] ++ (with unstable; [
   ]);
 }
