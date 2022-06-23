77d8d6f956bb3ca33cbc128e8855cf4b0c9fc2d6
Andre Perry <andre@vspry.com>
2022-06-23 01:05:11 +0000

    feat: add envconfig and -h flag

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"net"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/kelseyhightower/envconfig"
 	"go.uber.org/zap"
 
 	_ "cloud.google.com/go/firestore"
@@ -16,7 +21,6 @@ import (
 	_ "github.com/golang-jwt/jwt/v4"
 	_ "github.com/google/uuid"
 	_ "github.com/gorilla/sessions"
-	_ "github.com/kelseyhightower/envconfig"
 	_ "github.com/microcosm-cc/bluemonday"
 	_ "github.com/nyaruka/phonenumbers"
 	_ "github.com/stretchr/testify/assert"
@@ -26,34 +30,80 @@ import (
 	_ "google.golang.org/grpc"
 )
 
-const ()
+const (
+	APP = "go_skeleton"
+)
 
 var (
 	CommitSHA = "development" // nolint: gochecknoglobals
 	BuildID   = "development" // nolint: gochecknoglobals
 )
 
-type config struct{}
-
-func (c *config) ListenAddress() string {
-	return ""
+type Config struct {
+	Username string `required:"true"`
+	Password string `required:"true"`
+	Port     uint16 `default:"8080"`
+	Address  string `default:"127.0.0.1"`
+	mux      *chi.Mux
+	log      *zap.Logger
 }
 
-func (c *config) Router() *chi.Mux {
-	return chi.NewMux()
+func (c *Config) ListenAddress() string {
+	const base10 = 10
+
+	return net.JoinHostPort(c.Address, strconv.FormatUint(uint64(c.Port), base10))
+}
+
+func NewConfig() (*Config, error) {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		return nil, fmt.Errorf("could not create a zap development logger: %w", err)
+	}
+
+	config := new(Config)
+	config.mux = chi.NewMux()
+	config.log = logger
+
+	flag.Usage = func() {
+		if err := envconfig.Usage(APP, config); err != nil {
+			return
+		}
+	}
+	flag.Parse()
+
+	err = envconfig.Process(APP, config)
+	if err != nil {
+		if err := envconfig.Usage(APP, config); err != nil {
+			return nil, fmt.Errorf("could not produce usage: %w", err)
+		}
+
+		return nil, fmt.Errorf("environment configuration error: %w", err)
+	}
+
+	return config, nil
+}
+
+func (c *Config) Sync() error {
+	if err := c.log.Sync(); err != nil {
+		return fmt.Errorf("could not sync log: %w", err)
+	}
+
+	return nil
+}
+
+func (c *Config) Router() *chi.Mux {
+	return c.mux
 }
 
 func main() {
-	conf := &config{}
-	var err error
-	logger := zap.S()
+	config, err := NewConfig()
 	if err != nil {
-		logger.Fatalf("error configuring: %v", err)
+		panic(err)
 	}
-	defer logger.Sync() // nolint: errcheck
+	defer config.Sync() // nolint: errcheck
 
-	logger.Infow("version", "commit", CommitSHA, "build", BuildID)
-
-	logger.Infow("starting server", "listen", conf.ListenAddress())
-	logger.Fatal(http.ListenAndServe(conf.ListenAddress(), conf.Router()))
+	log := config.log.Sugar()
+	log.Infow("version", "commit", CommitSHA, "build", BuildID)
+	log.Infow("starting server", "listen", config.ListenAddress())
+	log.Error(http.ListenAndServe(config.ListenAddress(), config.Router()))
 }
