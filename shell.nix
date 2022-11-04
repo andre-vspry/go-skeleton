24629094af684a925ef119347df25fada128c327
Andre Perry <andre@vspry.com>
2022-11-04 01:33:28 +0000

    chore: use overlay pre-commit-golang

--- shell.nix
+++ shell.nix
@@ -3,34 +3,11 @@ let
   go = pkgs.go_1_18;
   nix-pre-commit-hooks =
     import (builtins.fetchGit "https://github.com/cachix/pre-commit-hooks.nix");
-  pre-commit-golang = pkgs.callPackage ({ stdenv, fetchFromGitHub }:
-    stdenv.mkDerivation rec {
-      pname = "pre-commit-golang";
-      version = "0.5.0-96221dc";
-      src = fetchFromGitHub {
-        owner = "dnephin";
-        repo = pname;
-        rev = "96221dc741cb30cc0136999083dc6bd0e2113000";
-        sha256 = "sha256-lIQBoT+UIlVGnFaaGBgXag0Lm1UhGj/pIGlCCz91L4I=";
-      };
-      dontBuild = true;
-      dontConfigure = true;
-      installPhase = ''
-        runHook preInstall
-        install -d $out
-        find . -type f -name 'run-*.sh' \
-        | sed -E 's:^\./run-(.*)\.sh:\1:' \
-        | xargs -I {} install ./run-{}.sh $out/{}
-        runHook postInstall
-      '';
-    }) { };
   pre-commit-golang-hook = name: {
-    "${name}" = {
-      inherit name;
-      enable = true;
-      entry = "${pre-commit-golang}/${name}";
-      files = "\\.go$";
-    };
+    inherit name;
+    enable = true;
+    entry = "${pkgs.pre-commit-golang}/${name}";
+    files = "\\.go$";
   };
 
   # gqlgen = unstable.callPackage ./nix/gqlgen {}; # Uses plugins which are not working
@@ -50,13 +27,20 @@ in pkgs.mkShell {
       goplay
       goreleaser
       go-mockery
-      go-outline
+      go-outliner
       go-bindata
       golangci-lint
       gofumpt
       go-jwt
       go-enum
+      oapi-codegen
       openssl
+      oauth2l
+      gin
+      cloudsql-proxy
+      cachecmd
+      mailslurper
+      ifacemaker
       jq
       gojq
       nixfmt
@@ -65,6 +49,7 @@ in pkgs.mkShell {
       google-cloud-sdk
       # skaffold minikube kubectl
       sonar-scanner-cli
+      nodePackages.markdownlint-cli
     ] ++ (with unstable; [ ]);
   shellHook = ({
     pre-commit-check = nix-pre-commit-hooks.run {
@@ -72,21 +57,22 @@ in pkgs.mkShell {
       # If your hooks are intrusive, avoid running on each commit with a default_states like this:
       # default_stages = ["manual" "push"];
       hooks = {
+        markdownlint.enable = true;
         terraform-format.enable = true;
         shellcheck.enable = true;
         nix-linter.enable = true;
         nixfmt.enable = true;
-      } // (pre-commit-golang-hook "golangci-lint")
-        // (pre-commit-golang-hook "go-fmt")
-        // (pre-commit-golang-hook "go-mod-tidy")
-        // (pre-commit-golang-hook "go-imports");
+        golangci-lint = (pre-commit-golang-hook "golangci-lint") // {
+          pass_filenames = false;
+        };
+        go-fmt = pre-commit-golang-hook "go-fmt";
+        go-mod-tidy = pre-commit-golang-hook "go-mod-tidy";
+        go-imports = pre-commit-golang-hook "go-imports";
+        go-unit-tests = pre-commit-golang-hook "go-unit-tests";
+      };
     };
   }).pre-commit-check.shellHook;
 }
 
-# - id: go-fmt
-# - id: golangci-lint
-# - id: go-mod-tidy
-# - id: go-imports
 # - id: validate-toml
 # - id: go-unit-tests
